0d9cae5d13bbff32a1536ef51b8589acc44c659a
Albert 'Bash' Yumol <albert.yumol@gmail.com>
2020-03-22 15:16:27 +0800

    python review and exercises

---
+++ session-2/1_basic_exercises/exercise3.py
@@ -0,0 +1,12 @@
+# Challenge - Functions Exercise
+
+# Create a function named tripleprint that takes a string as a parameter
+# and prints that string 3 times in a row.
+# So if I passed in the string "hello",
+# it would print "hellohellohello"
+
+x = 'hello'
+def tripleprint(x):
+    return 3*str(x)
+
+tripleprint(x)
