ccabf0ee1fde333d67d29ce68580dbac7b5cf2c1
Albert 'Bash' Yumol <albert.yumol@gmail.com>
2020-08-14 21:24:34 +0800

    Add files via upload

---
+++ session-5/challenge_1.py
@@ -0,0 +1,13 @@
+from time import sleep
+
+def delay(seconds):
+    print(f'Sleeping for {seconds} seconds(s)')
+    sleep(seconds)
+
+def main():
+    for i in range(5):
+        print(i)
+        delay(seconds=2)
+
+if __name__=='__main__':
+    main()
